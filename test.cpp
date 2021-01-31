f97058db8f89a4381276a2b5e4cdbc73769ffb53
Saif Mahmud Parvez <saif.mahmud.parvez@gmail.com>
2021-01-31 11:42:18 +0600

    11. test.cpp added for testing purpose

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int reverse(int a)
+{
+    int out=0;
+    int ten=0;
+    while (a>0)
+    {
+        int temp = a%10;
+        cout<<"temp "<<temp<<" ";
+        out = out*10;
+        out = out+temp;
+        a=a/10;
+
+    }
+
+    return out;
+
+}
+
+int main()
+{
+    int a;
+    cin>>a;
+    int rev = reverse(a);
+    cout<<a<<endl<<rev;
+
+}
